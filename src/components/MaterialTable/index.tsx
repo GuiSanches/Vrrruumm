62a02189229db713e43589acec6c02232fc12e97
Guilherme Sanches Flôres Queiroz <si.guilherme_queiroz@usp.br>
2022-06-30 11:50:26 -0300

    Add table

--- src/components/MaterialTable/index.tsx
+++ src/components/MaterialTable/index.tsx
@@ -1,12 +1,25 @@
 import { FC } from "react";
+import MUIDataTable from "mui-datatables";
 import { Container } from "./styles";
 
 
 const Table: FC = () => {
+    const columns = ["Name", "Company", "City", "State"];
+
+    const data = [
+        ["Joe James", "Test Corp", "Yonkers", "NY"],
+        ["John Walsh", "Test Corp", "Hartford", "CT"],
+        ["Bob Herm", "Test Corp", "Tampa", "FL"],
+        ["James Houston", "Test Corp", "Dallas", "TX"],
+    ];
 
     return (
         <Container>
-            Ola
+            <MUIDataTable
+                title={"Employee List"}
+                data={data}
+                columns={columns}
+            />
         </Container>
     )
 }
--- src/components/MaterialTable/styles.ts
+++ src/components/MaterialTable/styles.ts
@@ -2,4 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     max-width: 100%;
+    padding: 0 1em;
+    margin-top: 2em;
 `
