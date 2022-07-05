3d43e0f160ae89f6a5cefc584a53ddecfb27a3b7
Guilherme Sanches Flôres Queiroz <si.guilherme_queiroz@usp.br>
2022-07-05 19:25:21 -0300

    Add buttons

--- src/components/UserActions/AddPilot/index.tsx
+++ src/components/UserActions/AddPilot/index.tsx
@@ -1,7 +1,10 @@
-import { FC, useState } from "react";
+import { FC, useContext, useState } from "react";
+import { User, UserContextType } from "../../../context/@types.App";
+import { UserContext } from "../../../context/AppCtx";
 import { Submit, Content, Input, InputGroup, Label } from "../styles";
 
 export const AddPilot: FC = () => {
+    const { user } = useContext(UserContext) as UserContextType;
     const [driverRef, setDriverRef] = useState('');
     const [number, setNumber] = useState('');
     const [code, setCode] = useState('');
@@ -10,11 +13,51 @@ export const AddPilot: FC = () => {
     const [date, setDate] = useState('');
     const [Nationality, setNationality] = useState('');
 
+    const handleSubmit = () => {
+        if(user) {
+            console.log(user)
+            const userJson : User = {
+                user_id: user.user_id,
+                id_constructor: user.id_constructor,
+                id_driver: user.id_driver,
+                tipo: user.tipo,    
+            }
+    
+            const pil = {
+                driver_ref: driverRef,
+                number,
+                code,
+                forename: foreName,
+                surname,
+                date_of_birth: date,
+                nationality: Nationality    
+            }
+
+            console.log(JSON.stringify({
+                user: userJson,
+                pil
+            }))
+
+            fetch(`http://localhost:8000/api/pilotos`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    user: userJson,
+                    pil
+                }),
+            }).then(console.log)
+            .then(() => alert('Usuário criado com sucesso'))
+            .catch((err) => alert('Não foi possível inserir. Verifique se o piloto já existe'));
+        }
+    }
+
     return (
         <Content>
             <InputGroup>
                 <Label>DriverRef:</Label>
-                <Input type="text" placeholder="Digite aqui" value={driverRef} onChange={e => setDriverRef(e.target.value)}/>
+                <Input type="text" placeholder="Digite aqui" value={driverRef} onChange={e => setDriverRef(e.target.value)} />
             </InputGroup>
 
             <InputGroup>
@@ -47,7 +90,7 @@ export const AddPilot: FC = () => {
                 <Input type="text" placeholder="Digite aqui" value={Nationality} onChange={e => setNationality(e.target.value)} />
             </InputGroup>
 
-            <Submit>Salvar</Submit>
+            <Submit onClick={e => handleSubmit()}>Salvar</Submit>
 
         </Content>
     );
--- src/components/UserActions/AddTeam/index.tsx
+++ src/components/UserActions/AddTeam/index.tsx
@@ -1,12 +1,52 @@
-import { FC, useState } from "react";
+import { FC, useContext, useState } from "react";
+import { UserContextType, User } from "../../../context/@types.App";
+import { UserContext } from "../../../context/AppCtx";
 import { Submit, Content, Input, InputGroup, Label } from "../styles";
 
 
 export const AddTeam: FC = () => {
+    const { user } = useContext(UserContext) as UserContextType;
     const [constructorRef, setConstructorRef] = useState('');
     const [name, setName] = useState('');
     const [nationality, setNationality] = useState('');
     const [url, setUrl] = useState('');
+
+    const handleSubmit = () => {
+        if(user) {
+            console.log(user)
+            const userJson : User = {
+                user_id: user.user_id,
+                id_constructor: user.id_constructor,
+                id_driver: user.id_driver,
+                tipo: user.tipo,    
+            }
+    
+            const esc = {
+                constructor_ref: constructorRef,
+                name,
+                nationality,
+                url  
+            }
+
+            console.log(JSON.stringify({
+                user: userJson,
+                esc
+            }))
+
+            fetch(`http://localhost:8000/api/escuderias`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    user: userJson,
+                    esc
+                }),
+            }).then(console.log)
+            .then(() => alert('Time criado com sucesso'))
+            .catch((err) => alert('Não foi possível inserir. Verifique se o time já existe'));
+        }
+    }
 
     return (
         <Content>
