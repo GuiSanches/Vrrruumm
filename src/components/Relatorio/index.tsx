336e26d2933a30100b3cbad4af16f470c32f9143
Jonattan Willian <63685673+Th3Jow@users.noreply.github.com>
2022-07-05 15:24:21 -0300

    admin relatorio 1

--- src/components/Relatorio/index.tsx
+++ src/components/Relatorio/index.tsx
@@ -1,19 +1,37 @@
 import Router from "next/router";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { User } from "../../context/@types.App";
 import Actions from "../Actions";
 import Table from "../MaterialTable";
 import { Button, Container } from "./styles";
+import getRelatorios from "../../../pages/api/adminRelatorio";
 
 interface Props {
   user: User;
 }
 
 const Relatorio: FC<Props> = ({ user }) => {
+  const [relatorio, setRelatorio] = useState(1);
+  const [data, setData] = useState<Array<any>>([]);
+  const [columns, setColumns] = useState<Array<string>>([]);
+  const [title, setTitle] = useState<string>("");
+
+  useEffect(() => {
+    const fetching = async () => {
+      setData(await getRelatorios(user));
+    };
+    fetching();
+    if (user?.tipo === "Administrador") {
+      if (relatorio === 1) {
+        setColumns(["Status", "Quantidade"]);
+        setTitle("Quantidade por Status");
+      }
+    }
+  }, [relatorio]);
   return (
     <Container>
-      <Actions />
-      <Table />
+      <Actions onChange={(r) => setRelatorio(r)} />
+      <Table title={title} data={data} columns={columns} />
       <Button onClick={() => Router.back()}>Home</Button>
     </Container>
   );
